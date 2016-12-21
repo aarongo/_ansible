e0bc9c7c647b09e84797264469e05b8d662cc4df
EdwardLiu <lonnyliu@126.com>
2016-12-21 11:58:38 +0800

    Update global.sh

--- playbooks/roles/run_script/files/global.sh
+++ playbooks/roles/run_script/files/global.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+export JAVA_HOME=/software/jdk1.7.0_51
 script_path=/software/script/monitor
 out_dir=/tmp
 Now=$(date +%Y-%m-%d_%H-%M)
 IP_ADDR=$(/sbin/ifconfig eth1 | grep "inet addr" | awk '{print $2}' | awk -F ":" '{print $2}')
+hostname=$(/bin/hostname)
+dnsname="f.cdc.carrefour.com"
+FNQ=${hostname}${dnsname}
 upload_dir=/software/picture_upload/log_analysis/
 
 
@@ -13,24 +17,24 @@ at_tomcat(){
 }
 at_tomcat_handle(){
 	cd ${out_dir}
-	tar czvf ${IP_ADDR}_${Now}_tomcat.tar.gz jstack linux
-	mv ${IP_ADDR}_${Now}_tomcat.tar.gz ${upload_dir}
+	tar czvf ${FNQ}_${Now}_tomcat.tar.gz jstack linux
+	mv ${FNQ}_${Now}_tomcat.tar.gz ${upload_dir}
 	cd ${out_dir};rm -rf jstack linux
 }
 
 at_memcached(){
 	${script_path}/memcache.sh
 	cd ${out_dir}
-	tar czvf ${IP_ADDR}_${Now}_memcacehd.tar.gz memcached
-	mv ${IP_ADDR}_${Now}_memcacehd.tar.gz ${upload_dir}
+	tar czvf ${FNQ}_${Now}_memcacehd.tar.gz memcached
+	mv ${FNQ}_${Now}_memcacehd.tar.gz ${upload_dir}
 	cd ${out_dir};rm -rf memcached
 }
 
 at_mongodb(){
 	${script_path}/mongodb.sh
 	cd ${out_dir}
-	tar czvf ${IP_ADDR}_${Now}_mongodb.tar.gz mongodb
-	mv ${IP_ADDR}_${Now}_mongodb.tar.gz ${upload_dir}
+	tar czvf ${FNQ}_${Now}_mongodb.tar.gz mongodb
+	mv ${FNQ}_${Now}_mongodb.tar.gz ${upload_dir}
 	cd ${out_dir};rm -rf mongodb
 
 }
@@ -38,7 +42,7 @@ at_mongodb(){
 at_mysql(){
 	${script_path}/mysql.sh
 	cd ${out_dir}
-	tar czvf ${IP_ADDR}_${Now}_mysql.tar.gz mysql
+	tar czvf ${FNQ}_${Now}_mysql.tar.gz mysql
 	cd ${out_dir};rm -rf mysql
 }
 
